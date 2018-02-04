bf3021957283860f9db3a8d3ff3ffac4fa7e452d
smartkarthik <35652317+smartkarthik@users.noreply.github.com>
2018-02-04 08:54:15 +0000

    Create hello word n times.c

---
+++ hello word n times.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+     int a,i=1;
+     printf("\n printed hello");
+     scanf("%d",&a);
+     for(i=0;i<=a;i++)
+     {
+         printf("\n HELLO");
+         
+     }
+}
